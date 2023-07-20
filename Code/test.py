3220a8885228741911df2cc08f375ede49414924
Lekhana M <93895818+lekhana-m@users.noreply.github.com>
2023-07-21 00:42:29 +0530

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -12,7 +12,7 @@ imgSize = 300
 
 counter = 0
 
-labels = ["A","B", "C", "D", "E","G","M","P","Q","S","V", "W", "Y", "Z"]
+labels = ["A","B", "C", "D", "E","F","G","M","P","Q","S","V", "W", "Y", "Z"]
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
